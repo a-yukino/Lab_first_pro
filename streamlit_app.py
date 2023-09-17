ac429286b1197479808102ab408e6353da9dddac
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-17 23:10:52 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,4 @@ import streamlit as st
 
 st.title("女子FIVB選手のデータ分析ツール") # タイトル
 
-st.write("分析したいファイルをアップロードしてください。")
-st.file_uploader("Choose file") # ファイルアップロード
+uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
