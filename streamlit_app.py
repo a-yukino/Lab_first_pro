063ccb5df4462f54e698b334f1dbae7f61b0527a
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-17 22:47:02 +0900

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit as st
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+
+st.title("title") # タイトル
+st.header("header") # ヘッダー
+st.write("write") # 表示
+st.markdown("# markdown") # マークダウンで表示
+st.text("text") # テキスト表示
