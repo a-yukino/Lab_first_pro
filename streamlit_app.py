5a7477d58b345f32f58cf9e46e1bfbd53563b28e
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-18 13:32:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df1 = pd.read_csv('clean_data.csv',encoding='shift_jis')
+df = pd.read_csv('clean_data.csv',encoding='shift_jis')
 
 st.title('バレーボールW杯出場女子選手のデータ一覧')
-st.write(df1)
+st.write(df)
 
 st.write('国別のデータ')
 
