2242191e4b58b3d8da528009c1dcf3384161cd05
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-20 01:41:58 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,190 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-# CSVファイルの読み込み
-@st.cache  # データをキャッシュして高速化
+df = pd.read_csv('clean_data.csv',encoding='shift_jis')
+
+st.title('バレーボールW杯出場女子選手のデータ一覧')
+st.write(df)
+
+st.write('国別のデータ')
+
+@st.cache  
 def load_data():
-    data = pd.read_csv('pro_1.csv', encoding='utf-8')  # CSVファイルのパスを指定
+    data = pd.read_csv('pro_1.csv', encoding='utf-8')  
     return data
-
 data = load_data()
-
-# ボタンの表示
-if st.button("データを表示/非表示"):
+if st.button("ロシア"):
     if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  # 初回クリック時に変数を初期化
-    st.session_state.show_data = not st.session_state.show_data  # ステートを切り替え
-
-# データの表示/非表示
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
 if st.session_state.show_data:
-    st.write("CSVデータの表示:")
+    st.write("ロシアの代表選手のデータ")
     st.write(data)
 else:
-    st.write("CSVデータは非表示です。")
+    st.write("　")
+
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_2.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("ブラジル"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("ブラジルの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+    
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_3.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("日本"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("日本の代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_4.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("ブルガリア"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("ブルガリアの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+    
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_5.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("セルビア"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("セルビアの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+    
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_6.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("メキシコ"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("メキシコの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+    
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_7.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("キューバ"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("キューバの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_8.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("中国"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("中国の代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+    
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_9.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("エジプト"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("エジプトの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_10.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("ペルー"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("ペルーの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_11.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("イタリア"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("イタリアの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
+
+@st.cache  
+def load_data():
+    data = pd.read_csv('pro_12.csv', encoding='utf-8')  
+    return data
+data = load_data()
+if st.button("トルコ"):
+    if 'show_data' not in st.session_state:
+        st.session_state.show_data = True  
+    st.session_state.show_data = not st.session_state.show_data  
+if st.session_state.show_data:
+    st.write("トルコの代表選手のデータ")
+    st.write(data)
+else:
+    st.write("　")
