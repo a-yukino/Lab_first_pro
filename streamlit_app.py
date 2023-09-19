64a629cc83f8d6747b1195ee4dd0a583ec1a3c58
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-19 13:49:50 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,49 +10,49 @@ st.write(df)
 st.write('国別のデータ')
 
 if st.button('ロシア'):
-    df = pd.read_csv('pro_1.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_1.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('ブラジル'):
-    df = pd.read_csv('pro_2.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_2.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('日本'):
-    df = pd.read_csv('pro_3.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_3.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('ブルガリア'):
-    df = pd.read_csv('pro_4.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_4.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('セルビア'):
-    df = pd.read_csv('pro_5.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_5.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('メキシコ'):
-    df = pd.read_csv('pro_6.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_6.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('キューバ'):
-    df = pd.read_csv('pro_7.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_7.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('中国'):
-    df = pd.read_csv('pro_8.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_8.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('エジプト'):
-    df = pd.read_csv('pro_9.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_9.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('ペルー'):
-    df = pd.read_csv('pro_10.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_10.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('イタリア'):
-    df = pd.read_csv('pro_11.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_11.csv',encoding='utf-8')
     st.write(df)
 
 if st.button('トルコ'):
-    df = pd.read_csv('pro_12.csv',encoding='shift_jis')
+    df = pd.read_csv('pro_12.csv',encoding='utf-8')
     st.write(df)
