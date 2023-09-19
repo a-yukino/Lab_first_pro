1776af86f0a2c5c695d3ecfb0595f6189734b16f
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-20 03:02:26 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,40 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-# CSVファイルの読み込み
+df = pd.read_csv('clean_data.csv',encoding='shift_jis')
+
+st.title('バレーボールW杯出場女子選手のデータ一覧')
+st.write(df)
+
+st.write('国別のデータ')
+
 @st.cache
 def load_data():
     data = pd.read_csv('pro_1.csv', encoding='utf-8')
     return data
-
 data = load_data()
-
-# ボタンのクリック状態を管理する変数
 if 'show_data' not in st.session_state:
     st.session_state.show_data = False
-
-# ボタンでデータの表示/非表示を切り替え
 if st.button("ロシア"):
     st.session_state.show_data = not st.session_state.show_data  # クリックごとに表示状態をトグル
-
-# データの表示/非表示
 if st.session_state.show_data:
     st.write("ロシア代表のデータ")
+    st.write(data)
+else:
+    st.write("")
+
+@st.cache
+def load_data():
+    data = pd.read_csv('pro_2.csv', encoding='utf-8')
+    return data
+data = load_data()
+if 'show_data' not in st.session_state:
+    st.session_state.show_data = False
+if st.button("ブラジル"):
+    st.session_state.show_data = not st.session_state.show_data  # クリックごとに表示状態をトグル
+if st.session_state.show_data:
+    st.write("ブラジル代表のデータ")
     st.write(data)
 else:
     st.write("")
