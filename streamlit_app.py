29cc27efb72328f3095e41ec9e65d003b6ac19d2
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-20 01:46:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("ロシア"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data1' not in st.session_state:
+        st.session_state.show_data1 = True  
+    st.session_state.show_data1 = not st.session_state.show_data1  
+if st.session_state.show_data1:
     st.write("ロシアの代表選手のデータ")
     st.write(data)
 else:
@@ -30,10 +30,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("ブラジル"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data2' not in st.session_state:
+        st.session_state.show_data2 = True  
+    st.session_state.show_data2 = not st.session_state.show_data2  
+if st.session_state.show_data2:
     st.write("ブラジルの代表選手のデータ")
     st.write(data)
 else:
@@ -45,10 +45,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("日本"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data3' not in st.session_state:
+        st.session_state.show_data3 = True  
+    st.session_state.show_data3 = not st.session_state.show_data3  
+if st.session_state.show_data3:
     st.write("日本の代表選手のデータ")
     st.write(data)
 else:
@@ -60,10 +60,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("ブルガリア"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data4' not in st.session_state:
+        st.session_state.show_data4 = True  
+    st.session_state.show_data4 = not st.session_state.show_data4  
+if st.session_state.show_data4:
     st.write("ブルガリアの代表選手のデータ")
     st.write(data)
 else:
@@ -75,10 +75,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("セルビア"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data5' not in st.session_state:
+        st.session_state.show_data5 = True  
+    st.session_state.show_data5 = not st.session_state.show_data5  
+if st.session_state.show_data5:
     st.write("セルビアの代表選手のデータ")
     st.write(data)
 else:
@@ -90,10 +90,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("メキシコ"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data6' not in st.session_state:
+        st.session_state.show_data6 = True  
+    st.session_state.show_data6 = not st.session_state.show_data6 
+if st.session_state.show_data6:
     st.write("メキシコの代表選手のデータ")
     st.write(data)
 else:
@@ -105,10 +105,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("キューバ"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data7' not in st.session_state:
+        st.session_state.show_data7 = True  
+    st.session_state.show_data7 = not st.session_state.show_data7  
+if st.session_state.show_data7:
     st.write("キューバの代表選手のデータ")
     st.write(data)
 else:
@@ -120,10 +120,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("中国"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data8' not in st.session_state:
+        st.session_state.show_data8 = True  
+    st.session_state.show_data8 = not st.session_state.show_data8  
+if st.session_state.show_data8:
     st.write("中国の代表選手のデータ")
     st.write(data)
 else:
@@ -135,10 +135,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("エジプト"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data9' not in st.session_state:
+        st.session_state.show_data9 = True  
+    st.session_state.show_data9 = not st.session_state.show_data9  
+if st.session_state.show_data9:
     st.write("エジプトの代表選手のデータ")
     st.write(data)
 else:
@@ -150,10 +150,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("ペルー"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data10' not in st.session_state:
+        st.session_state.show_data10 = True  
+    st.session_state.show_data10 = not st.session_state.show_data10  
+if st.session_state.show_data10:
     st.write("ペルーの代表選手のデータ")
     st.write(data)
 else:
@@ -165,10 +165,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("イタリア"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data11' not in st.session_state:
+        st.session_state.show_data11 = True  
+    st.session_state.show_data11 = not st.session_state.show_data11  
+if st.session_state.show_data11:
     st.write("イタリアの代表選手のデータ")
     st.write(data)
 else:
@@ -180,10 +180,10 @@ def load_data():
     return data
 data = load_data()
 if st.button("トルコ"):
-    if 'show_data' not in st.session_state:
-        st.session_state.show_data = True  
-    st.session_state.show_data = not st.session_state.show_data  
-if st.session_state.show_data:
+    if 'show_data12' not in st.session_state:
+        st.session_state.show_data12 = True  
+    st.session_state.show_data12 = not st.session_state.show_data12  
+if st.session_state.show_data12:
     st.write("トルコの代表選手のデータ")
     st.write(data)
 else:
