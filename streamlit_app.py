8fc9d0466d671f68398ee0000c621780f71544f5
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-20 02:12:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,20 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-df = pd.read_csv('clean_data.csv',encoding='shift_jis')
-
-st.title('バレーボールW杯出場女子選手のデータ一覧')
-st.write(df)
-
-st.write('国別のデータ')
-
+# セッション変数を初期化
 if 'button1_clicked' not in st.session_state:
-    st.session_state.button1_clicked = pd.read_csv('pro_1.csv',encoding='utf-8')
-    if st.button("ロシア"):
-        st.session_state.button1_clicked = not st.session_state.button1_clicked
-    st.write(f"ロシアの代表選手のデータ: {st.session_state.button1_clicked}")
-    
+    st.session_state.button1_clicked = False
+
 if 'button2_clicked' not in st.session_state:
-    st.session_state.button2_clicked = pd.read_csv('pro_2.csv',encoding='utf-8')
-    if st.button("ブラジル"):
-        st.session_state.button2_clicked = not st.session_state.button2_clicked
-    st.write(f"ブラジルの代表選手のデータ: {st.session_state.button2_clicked}")
+    st.session_state.button2_clicked = False
+
+# ボタン1
+if st.button("ボタン1"):
+    st.session_state.button1_clicked = not st.session_state.button1_clicked
+
+# ボタン2
+if st.button("ボタン2"):
+    st.session_state.button2_clicked = not st.session_state.button2_clicked
+
+# ボタンのクリック状態を表示
+st.write(f"ボタン1のクリック状態: {st.session_state.button1_clicked}")
+st.write(f"ボタン2のクリック状態: {st.session_state.button2_clicked}")
