b6d920cd1a780efda4daa282e02cb2936ab83d0d
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-20 02:51:54 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,17 @@ import pandas as pd
 def load_data():
     data = pd.read_csv('pro_1.csv', encoding='utf-8')
     return data
+
 data = load_data()
+
+# ボタンのクリック状態を管理する変数
 show_data = False
-if st.button("ロシア"):
-    show_data = not show_data  
-    
+
+# ボタンでデータの表示/非表示を切り替え
+if st.button("データを表示/非表示"):
+    show_data = not show_data  # クリックごとに表示状態をトグル
+
+# データの表示/非表示
 if show_data:
     st.write("CSVデータの表示:")
     st.write(data)
