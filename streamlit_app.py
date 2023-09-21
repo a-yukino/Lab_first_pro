10d8127337ff543102f5fc14c655b1515b5f0c36
芦原千乃 <130324775+a-yukino@users.noreply.github.com>
2023-09-21 18:20:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,13 @@
-import streamlit as st
-import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-# データの読み込み
-@st.cache  # データをキャッシュして高速化
-def load_data():
-    data = pd.read_csv('pro_1.csv')  # データファイルのパスを指定
-    return data
+import streamlit as st
+import matplotlib.pyplot as plt
 
-data = load_data()
+# Matplotlibでグラフを作成
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3], [4, 5, 6])
 
-# サイドバーにタイトルを表示
-st.sidebar.title('データ分析アプリ')
-
-# サイドバーでデータの表示/非表示を制御
-if st.sidebar.checkbox('データを表示'):
-    st.subheader('データの概要')
-    st.write(data)
-
-# サイドバーで散布図の表示/非表示を制御
-if st.sidebar.checkbox('散布図を表示'):
-    st.subheader('散布図')
-    x_axis = st.selectbox('X軸を選択', data.columns)
-    y_axis = st.selectbox('Y軸を選択', data.columns)
-
-    # 散布図の作成
-    plt.figure(figsize=(10, 6))
-    plt.scatter(data[x_axis], data[y_axis])
-    plt.xlabel(x_axis)
-    plt.ylabel(y_axis)
-    st.pyplot(plt)
+# Streamlitに表示
+st.pyplot(fig)
